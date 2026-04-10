f7718045c763d5dd9ca0ce8590ee4cbfdc3171ea
e011w <87497936+e011w@users.noreply.github.com>
2026-04-10 23:34:49 +0300

    feat: Integrate Capacitor for native builds
    Adds Capacitor dependencies and configuration to enable building native Android applications. Includes necessary scripts for syncing, opening, and building the native project.

---
+++ android/app/src/main/java/com/orsan/services/MainActivity.java
@@ -0,0 +1,5 @@
+package com.orsan.services;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.orsan.services',
+  appName: 'OrsanServices',
+  webDir: 'dist'
+};
+
+export default config;
